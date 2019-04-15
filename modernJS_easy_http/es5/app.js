2610cb6cb421ac64520d573ba004b55cdc0eced5
w3althambition <georgicodes@outlook.com>
2019-04-15 18:42:18 +0100

    easyHTML lib tutorial complated

--- modernJS_easy_http/es5/app.js
+++ modernJS_easy_http/es5/app.js
@@ -1,12 +1,66 @@
 /* eslint-disable func-names, no-undef */
 
+// Instantiate our lib.
 const http = new EasyHTTP();
 
-// Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', (err, res) => {
+// Sample data.
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post',
+};
+
+/*
+// Get Posts from a server
+*/
+
+// http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+/*
+// Get single post
+*/
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1', (err, post) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+/*
+// Update a post on the a server.
+*/
+
+// http.post('https://jsonplaceholder.typicode.com/posts', data, (err, post) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, (err, post) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(res);
+    console.log(post);
+  }
+});
+
+/*
+// Push post to a server.
+*/
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', (err, posts) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(posts);
   }
 });
