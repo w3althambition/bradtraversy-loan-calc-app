4f6b2f65007d2bb9d0d9479769c0aa198e789a62
w3althambition <georgicodes@outlook.com>
2019-10-02 09:00:02 +0100

    backend for apex elegends tutorial complated

---
+++ 21_apex_legends_tracker_app/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// load env
+dotenv.config({
+	path: './config.env',
+});
+
+const app = express();
+
+// dev logging
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+// profile routes
+app.use('/api/v1/profile', require('./routes/profile'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
