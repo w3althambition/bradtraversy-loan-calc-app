01c43f21803d36047fe9ff27627f4993a97208b3
w3althambition <georgicodes@outlook.com>
2019-10-03 16:56:59 +0100

    progress on apex legends tutorial by Brad Traversy

---
+++ 21_apex_legends_tracker_app/client/src/components/Profile.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		Profile
+	</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+	name: "Profile",
+	data() {
+		return {
+			error: null,
+			loading: false,
+			profileData: null
+		};
+	},
+	beforeCreate() {
+		document.body.className = "body-bg-no-image";
+	},
+	async created() {
+		this.loading = true;
+
+		try {
+			const responsive = await axios.get(
+				`http://localhost:5000/app/v1/profile/${this.$route.params.platform}/${this.$route.params.gamertag}`
+			);
+
+			this.profileData = res.data.data;
+			console.log(this.profileData);
+      this.loading = false;
+
+		} catch (err) {
+			this.loading = false;
+			this.error = err.responsive.data.message;
+		}
+	}
+};
+</script>
